2b438e9357a8a0f31225ff6a09b0e1e323f19573
Caroline <carolineho1983@gmail.com>
2023-05-28 18:11:56 +0100

    Added extra prompts, renamed triggers

--- src/chatbot-rec-steps.js
+++ src/chatbot-rec-steps.js
@@ -5,43 +5,53 @@ const recruitmentPrompts = [
     id: "recruitment",
     // TODO: add a settimeout between 2 sentences
     message: `can't wait to hear what you have to offer. Any questions?`,
-    trigger: "recQuestions",
+    trigger: "rec-options",
   },
   {
-    id: "recQuestions",
+    id: "rec-options",
     options: [
       {
         value: "salary expectation",
         label: "salary expectation",
-        trigger: "salary expectation",
+        trigger: "salary-expectation",
       },
       {
         value: "notice period",
         label: "notice period",
-        trigger: "notice period",
+        trigger: "notice-period",
       },
       {
         value: "work style",
         label: "work style",
-        trigger: "work style",
+        trigger: "work-style",
+      },
+      {
+        value: "right to work in UK",
+        label: "right to work in UK",
+        trigger: "rights-work",
       },
     ],
   },
 
   {
-    id: "salary expectation",
+    id: "salary-expectation",
     message: `salary expected per year: multiply the magic number by 1000. Please select a new topic`,
-    trigger: "recQuestions",
+    trigger: "rec-options",
   },
   {
-    id: "notice period",
+    id: "notice-period",
     message: `notice period : 3 months, might be negotiable. Please select a new topic`,
-    trigger: "recQuestions",
+    trigger: "rec-options",
   },
   {
-    id: "work style",
+    id: "work-style",
     message: `I am open to Hybrid up to 2 days at the office / week maximum, or fully remote. Any other questions?`,
-    trigger: "recQuestions",
+    trigger: "rec-options",
+  },
+  {
+    id: "rights-work",
+    message: `Don't worry! I have the settled status. I have unlimited right to work in the UK.Any other questions?`,
+    trigger: "rec-options",
   },
 ];
 
