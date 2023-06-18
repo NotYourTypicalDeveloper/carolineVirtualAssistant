00190e9dc856a26aca3364092b47b5f0e014e11a
Caroline <carolineho1983@gmail.com>
2023-06-18 17:26:06 +0100

    Updates

---
+++ precache-manifest.7438f13423f4c02839d9ab25e6ebea5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b6fce3a6bf06be3066b",
+    "url": "/caroline-virtual-assistant/static/css/main.9fdbd951.chunk.css"
+  },
+  {
+    "revision": "2b6fce3a6bf06be3066b",
+    "url": "/caroline-virtual-assistant/static/js/main.3615b1c8.chunk.js"
+  },
+  {
+    "revision": "172c1b98bc981c834a87",
+    "url": "/caroline-virtual-assistant/static/js/runtime~main.3dffb6d9.js"
+  },
+  {
+    "revision": "ece38b0a9d0c2cb0699a",
+    "url": "/caroline-virtual-assistant/static/js/2.6e1a0ee6.chunk.js"
+  },
+  {
+    "revision": "d0621eb3852492709e51899762552eb0",
+    "url": "/caroline-virtual-assistant/static/media/user1.d0621eb3.jpg"
+  },
+  {
+    "revision": "c2854fc291133cae153e9be5a97a8fac",
+    "url": "/caroline-virtual-assistant/static/media/chatbot-logo-bg.c2854fc2.jpg"
+  },
+  {
+    "revision": "5d4cdaf8bee528dcf6411d96e6349d69",
+    "url": "/caroline-virtual-assistant/index.html"
+  }
+];
