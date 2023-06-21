323e1307a6500a788ce43bcc966e43f6efc50fba
Caroline <carolineho1983@gmail.com>
2023-06-21 22:53:36 +0100

    Added more REC prompts and refactored existing code

--- src/REC-userinputs-steps.js
+++ src/REC-userinputs-steps.js
@@ -12,23 +12,28 @@ const rec_UserInputPrompts = [
   {
     id: "listening-to-input",
     message: ({ previousValue }) => {
+      const regexSkills = /skills.*current|current.*skills|tech.*stack/i;
+      const regexLocation = /(current|currently.*location|based|live)|(where.*live|located)/i;
+      const regexGreetings = /\b(hello|hi|howdy|oi|morning|Good afternoon|cheerio)\b/i;
+      const regexCV = /((get|send|email).*cv|resume)/i;
+      const regexQuit = /((reason|why).*(leaving|quitting|job))|(change|leave).*(company|job|role)/i;
+      const regexExpYears = /\byears\b.*\bexperience\b/i;
+
       const prevVal = previousValue.toLowerCase();
-      if (prevVal.includes("current skills") || prevVal.includes("skills")) {
-        return "My main skills are JavaScript, HTML, CSS, ReactJS, NextJS, chakra-ui, graphQL, API requests. and I have long history of Microsoft Sharepoint development including RPA Automation, pure JS, CSS, HTML, MS365 eco-system.";
-      } else if (
-        prevVal.includes("location") ||
-        prevVal.includes("where are you based")
-      ) {
+      if (regexSkills.test(prevVal)) {
+        return "My main skills are JavaScript, HTML, CSS, ReactJS, NextJS, chakra-ui, graphQL, API requests. and I have good experience of Microsoft Sharepoint development including RPA Automation, pure JS, CSS, HTML, MS365 eco-system.";
+      } else if (regexLocation.test(prevVal)) {
         return "I am based in North London, West Finchley";
-      } else if (prevVal.includes("hello") || prevVal.includes("hi")) {
+      } else if (regexGreetings.test(prevVal)) {
         return "Hello";
       } else if (prevVal.includes("current situation")) {
         return "I am currently employed at the Bike Club as a Front-end developer.";
-      } else if (
-        prevVal.includes("reason for leaving") ||
-        prevVal.includes("change job")
-      ) {
+      } else if (regexQuit.test(prevVal)) {
         return "I am looking for a more ambitous challenge and better opportunities to grow as a developer";
+      } else if (regexCV.test(prevVal)) {
+        return "sure, I will send it over. Meanwhile check my LinkedIn profile";
+      } else if (regexExpYears.test(prevVal)) {
+        return "I have 4 years of experience as a web developer.";
       } else {
         return "I dont understand";
       }
